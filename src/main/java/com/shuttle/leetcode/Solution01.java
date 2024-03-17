d12463e9322a879a36e843b5e9bad0925fcdb0bd
Shuttle <Shuttleupup@163.com>
2024-03-17 22:50:42 +0800

    feat: leetcode [01 & 31]

---
+++ src/main/java/com/shuttle/leetcode/Solution01.java
@@ -0,0 +1,66 @@
+package com.shuttle.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution01 {
+
+    /**
+     * 解法一：遍历
+     * 思路：对 nums 数组进行内外两次嵌套遍历，寻找外层和里层 数值和 == 目标值 的下标对返回
+     * 时间复杂度：O(N²) N 是 nums 数组中元素数量
+     * 空间复杂度：O(1) 没有额外创建数据结构
+     *
+     * @param nums   整数数组
+     * @param target 目标值
+     * @return 和为目标值的那两个整数在数组中的下标 [index1, index2]
+     */
+    public int[] twoSumSolution1(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return new int[0];
+        }
+
+        int numsLen = nums.length;
+
+        for (int i = 0; i < numsLen; i++) {
+            for (int j = i + 1; j < numsLen; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[0];
+    }
+
+    /**
+     * 解法二：HashMap
+     * 思路：对 nums 数组进行两次遍历，寻找外层和里层 数值和 == 目标值 的下标对返回
+     * 时间复杂度：O(N) 仅遍历一次 nums 数组
+     * 空间复杂度：O(N) 额外创建了一个 HashMap 来存储 num 和 index 的映射关系
+     *
+     * @param nums   整数数组
+     * @param target 目标值
+     * @return 和为目标值的那两个整数在数组中的下标 [index1, index2]
+     */
+    public int[] twoSumSolution2(int[] nums, int target) {
+        if (nums == null || nums.length < 2) {
+            return new int[0];
+        }
+        // <num, index>
+        Map<Integer, Integer> numToIndexMap = new HashMap<>();
+        int numsLen = nums.length;
+
+        for (int i = 0; i < numsLen; i++) {
+            int curNum = nums[i];
+            int diffNum = target - curNum;
+            if (numToIndexMap.containsKey(diffNum)) {
+                return new int[]{numToIndexMap.get(diffNum), i};
+            }
+            numToIndexMap.put(curNum, i);
+        }
+
+        return new int[0];
+    }
+
+}
---
+++ src/main/java/com/shuttle/leetcode/Solution31.java
@@ -0,0 +1,46 @@
+package com.shuttle.leetcode;
+
+public class Solution31 {
+
+    /**
+     * 思路：逆序找降序数对
+     * 时间复杂度：O(N) N 为 nums 数组的长度，最多只需要循环两次
+     * 空间复杂度：O(1) 没有额外创建数据结构
+     *
+     * @param nums 整数数组
+     */
+    public void nextPermutation(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        // 从后往前找第一个降序数对，descIndex 为降序数对的前一个元素索引值。如 [1, 2, 3] descIndex = 1
+        int descIndex = nums.length - 2;
+        while (descIndex >= 0 && nums[descIndex] >= nums[descIndex + 1]) {
+            descIndex--;
+        }
+        // 再从后往前找，需要找一个比 nums[descIndex] 大的数进行交换
+        if (descIndex >= 0) {
+            int ascIndex = nums.length - 1;
+            while (ascIndex >= 0 && nums[ascIndex] <= nums[descIndex]) {
+                ascIndex--;
+            }
+            swapTwoNum(nums, ascIndex, descIndex);
+        }
+        // 最后将 descIndex + 1 到结尾段进行反转
+        int endIndex = nums.length - 1;
+        descIndex++;
+
+        while (descIndex < endIndex) {
+            swapTwoNum(nums, endIndex, descIndex);
+            endIndex--;
+            descIndex++;
+        }
+    }
+
+    private void swapTwoNum(int[] nums, int src, int dest) {
+        int temp = nums[src];
+        nums[src] = nums[dest];
+        nums[dest] = temp;
+    }
+
+}
