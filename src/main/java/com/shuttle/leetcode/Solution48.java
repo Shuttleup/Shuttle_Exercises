6d0f56b22dd8f4d6b37805f1cf3c04dfda1fec65
Shuttle <Shuttleupup@163.com>
2024-03-18 23:59:17 +0800

    feat: leetcode [48 & 49]

---
+++ src/main/java/com/shuttle/leetcode/Solution48.java
@@ -0,0 +1,36 @@
+package com.shuttle.leetcode;
+
+public class Solution48 {
+
+    /**
+     * 思路：顺时针旋转 90° 等价于 水平翻转 + 主对角线翻转
+     *      逆时针旋转 90° 等价于 水平翻转 + 一条对角线翻转
+     * 时间复杂度：O(N²) N 是待旋转的二维数组 matrix 的长度
+     * 空间复杂度：O(1) 没有额外创建数据结构
+     *
+     * @param matrix 待旋转的二维数组
+     */
+    public void rotate(int[][] matrix) {
+        if (matrix == null || matrix[0].length == 0) {
+            return;
+        }
+        int matrixLen = matrix.length;
+        // 先水平翻转
+        for (int i = 0; i < matrixLen / 2; i++) {
+            for (int j = 0; j < matrixLen; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[matrixLen - i - 1][j];
+                matrix[matrixLen - i - 1][j] = temp;
+            }
+        }
+        // 再主对角线翻转
+        for (int i = 0; i < matrixLen; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/shuttle/leetcode/Solution49.java
@@ -0,0 +1,55 @@
+package com.shuttle.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution49 {
+
+    /**
+     * 思路：Word 中字符 + 数量组合作为异位词唯一键进行分组
+     * 时间复杂度：O(N * M) N 是字符串数组 strs 的长度，M 是数组中 word 的平均长度
+     * 空间复杂度：O(N) uniqueCharCountKeyToGroupAnagramsMap 和结果列表 anagramsGroupList
+     *
+     * @param strs 字符串数组
+     * @return 异位词分组列表
+     */
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> anagramsGroupList = new ArrayList<>();
+
+        if (strs == null || strs.length == 0) {
+            return anagramsGroupList;
+        }
+        // <wordUniqueCharCountKey, groupAnagrams>
+        Map<String, List<String>> uniqueCharCountKeyToGroupAnagramsMap = new HashMap<>();
+
+        for (String word : strs) {
+            int[] uniqueCharCounts = new int[26];
+            int wordLen = word.length();
+            // 统计当前 word 中每个字符出现的次数
+            for (int i = 0; i < wordLen; i++) {
+                uniqueCharCounts[word.charAt(i) - 'a']++;
+            }
+            // 将出现次数 > 0 的字符 + 次数拼接作为 HashKey
+            StringBuilder uniqueCharCountKeyBuilder = new StringBuilder();
+
+            for (int i = 0; i < 26; i++) {
+                if (uniqueCharCounts[i] > 0) {
+                    uniqueCharCountKeyBuilder
+                            .append((char) (i + 'a'))
+                            .append(uniqueCharCounts[i]);
+                }
+            }
+
+            String uniqueCharCountKey = uniqueCharCountKeyBuilder.toString();
+            List<String> groupAnagrams = uniqueCharCountKeyToGroupAnagramsMap.getOrDefault(uniqueCharCountKey, new ArrayList<>());
+            groupAnagrams.add(word);
+            uniqueCharCountKeyToGroupAnagramsMap.put(uniqueCharCountKey, groupAnagrams);
+        }
+        anagramsGroupList.addAll(uniqueCharCountKeyToGroupAnagramsMap.values());
+
+        return anagramsGroupList;
+    }
+
+}
